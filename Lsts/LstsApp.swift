74c29cdd7deb0341c53737d5df2879dddac6c933
Leo Dion <leogdion@brightdigit.com>
2021-07-21 08:49:48 -0400

    Initial Commit

---
+++ Lsts/LstsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LstsApp.swift
+//  Lsts
+//
+//  Created by Leo Dion on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct LstsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
