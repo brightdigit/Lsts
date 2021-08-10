a1c34c30145c32a56d45f44cc38199f0a205e15c
Leo Dion <leogdion@brightdigit.com>
2021-08-10 11:10:07 -0400

    adding api for lsts

--- LstsKit/Package.swift
+++ LstsKit/Package.swift
@@ -22,6 +22,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
       .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+      .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+      .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -35,7 +37,9 @@ let package = Package(
       .target(
           name: "LstsServerKit",
           dependencies: ["LstsKit",
-                         .product(name: "Vapor", package: "vapor")]),
+                         .product(name: "Vapor", package: "vapor"),
+                         .product(name: "Fluent", package: "fluent"),
+                           .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver")]),
         .target(name: "lstsd", dependencies: ["LstsServerKit"]),
         .testTarget(
             name: "LstsKitTests",
--- LstsKit/Sources/LstsServerKit/ServerApp.swift
+++ LstsKit/Sources/LstsServerKit/ServerApp.swift
@@ -1,4 +1,6 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 public struct ServerApp {
   internal init(env: Environment) {
@@ -23,9 +25,32 @@ public struct ServerApp {
     try app.run()
   }
   
- 
+  static let defaultPostgreSQLConfig = PostgresConfiguration(hostname: "localhost", username: "lsts")
+  
+  static func postgreSQLConfig(from environment: Environment.Type) -> PostgresConfiguration {
+    guard let url = environment.get("DATABASE_URL") else {
+      return defaultPostgreSQLConfig
+    }
+    return PostgresConfiguration(url: url)!
+  }
+  
+  static func databaseConfiguration (from environment: Environment.Type) -> (DatabaseConfigurationFactory, DatabaseID) {
+    return (.postgres(configuration: postgreSQLConfig(from: environment)), .psql)
+  }
+  
   public func configure() throws {
-
+    /// Don't forget to start your database via **docker**:
+    /// `docker run  --name fruta-pg -e POSTGRES_HOST_AUTH_METHOD=trust -d -p 5432:5432 postgres -c log_statement=all`
+    /// `psql -h localhost -U postgres < ./setup.sql`
+    let databaseConfiguration = Self.databaseConfiguration(from: Environment.self)
+    
+    self.app.databases.use(databaseConfiguration.0, as: databaseConfiguration.1)
+    app.migrations.add([
+      LstsItemMigration()
+    ])
+    try app.autoMigrate().wait()
+    
+    try self.app.register(collection: LstsItemController())
     
     try self.app.register(collection: HelloWorldController())
   }
